74c438485a98168f071f8f6dc459a11f763edcb5
asritha814 <11249A199@kanchiuniv.ac.in>
2025-11-26 17:42:19 +0530

    Create binary.cpp

---
+++ binary.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+    float real,imag;
+    public:
+    Complex(float r=0,float i=0)
+    {
+        real=r;
+        imag=i;
+    }
+    Complex operator+(Complex obj)
+    {
+        Complex temp;
+        temp.real=real+obj.real;
+        temp.imag=imag+obj.imag;
+        return temp;
+    }
+    void display()
+    {
+        cout<<real<<"+"<<imag<<"i"<<endl;
+    }
+};
+int main()
+{
+    Complex c1(3,4),c2(1,2);
+    Complex c3=c1+c2;
+    cout<<"first complex number:";
+    c1.display();
+    cout<<"Second complex number:";
+    c2.display();
+    cout<<"Sum of two complex number:";
+    c3.display();
+    return 0;
+}
+
+    
+        
+    
